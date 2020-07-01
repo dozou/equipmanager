13804a8726e44fd1b21468decdbaa32f09f10e86
Muromi <akadama13@fratelloworks.com>
2020-07-01 23:30:13 +0900

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <FileTable></FileTable>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FileTable from "./components/FileTable";
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    FileTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
